4bfb4b505280aac5a23eb0d7c47d9414ace38cb2
wangmi <87730066>
2020-07-27 11:02:01 +0800

    1.3.0 修正线程安排问题

--- commons-server/src/main/java/com/dili/commons/rabbitmq/ReissueTask.java
+++ commons-server/src/main/java/com/dili/commons/rabbitmq/ReissueTask.java
@@ -1,10 +1,13 @@
 package com.dili.commons.rabbitmq;
 
+import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -23,19 +26,22 @@ public class ReissueTask implements Runnable {
     @Override
 
     public void run() {
-        List<CorrelationDataExt> messageCorrelationDataList = new ArrayList<>(SendFailedMessageHolder.getAll());
-        logger.info("------------------获取到" + messageCorrelationDataList.size() + "条ack=false的消息，准备重发------------------");
-        SendFailedMessageHolder.clear();
+        HashSet all = SendFailedMessageHolder.getAll();
+        List<CorrelationDataExt> messageCorrelationDataList = new ArrayList<>(all);
+        all.removeAll(messageCorrelationDataList);
+        logger.info(String.format("------------------获取到%d条ack=false的消息，准备重发------------------", messageCorrelationDataList.size()));
         int i = 1;
         for (CorrelationDataExt messageCorrelationData : messageCorrelationDataList) {
             Object message = messageCorrelationData.getMessage();
             String messageId = messageCorrelationData.getId();
-            logger.info("------------------重发第" + i + "条消息，id: " + messageId + "------------------");
+            logger.info(String.format("------------------重发第%d条消息，id: %s------------------", i, messageId));
             i++;
             rabbitTemplate.convertSendAndReceive(messageCorrelationData.getExchange(), messageCorrelationData.getRoutingKey(),
                     message, messageCorrelationData);
         }
         logger.info("------------------重发完成------------------");
     }
+
+
 
 }
