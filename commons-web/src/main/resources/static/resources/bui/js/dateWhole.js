c5b0b3055555d5649d08baaf51308b32ea86bf36
fanger <chenliangfang@diligrp.com>
2020-12-15 10:10:57 +0800

    feat: 计算整年整月整日

---
+++ commons-web/src/main/resources/static/resources/bui/js/dateWhole.js
@@ -0,0 +1,73 @@
+/**
+ * 计算整年整月整日
+ * @startDate 开始日期
+ * endDate 结束日期
+*/
+
+function wholeDateCalc(startDate, endDate) {
+    var wholeYear = 0, wholeMonth = 0, WholeDay = 0;
+    var startYear = moment(startDate).get('year');
+    var startMonth = moment(startDate).get('month');
+    var startDay = moment(startDate).get('date');
+
+    var endYear = moment(endDate).get('year');
+    var endMonth = moment(endDate).get('month');
+    var endDay = moment(endDate).get('date');
+
+    if(startMonth == 0 && startDay == 1) {
+        if (endMonth == 11 && endDay == 31) {
+            // 1.1&&12.31 即年初年末时: 前后都取整年,直接计算整年其余为0;如2018-01-01&&2020-12-31
+            wholeYear = endYear - startYear + 1;
+            wholeMonth = 0;
+            WholeDay = 0;
+        } else if(endDay == moment(endDate).endOf('month').format("DD")){
+            // 1.1&&!12.31,&&月末, 即年初非年末但月末时: 前取整年后取月天为0, 如果当年0,跨1年1,跨2年为2;如2018-01-01&&2020-11-30
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear;
+            wholeMonth = endMonth + 1;
+            WholeDay = 0;
+        } else {
+            // 1.1&&!12.31,&&非月末, 即年初非年末非月末时: 如2018-01-01&&2020-11-03
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear;
+            wholeMonth = endMonth;
+            WholeDay = endDay;
+        }
+    }else if(startDay == 1) {
+        if (endMonth == 11 && endDay == 31) {
+            // 非年初但月初且年末: 前取月天0,后取年 ;如2018-02-01&&2020-12-31
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear
+            wholeMonth = 12 - startMonth;
+            WholeDay = 0;
+        } else if(endDay == moment(endDate).endOf('month').format("DD")){
+            // 非年初但月初且非年末但月末 前后都取月中间取年, 如果当年0,跨1年1,跨2年为2;如2018-02-01&&2020-11-30
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear
+            wholeMonth = (12 - startMonth) + (endMonth + 1);
+            WholeDay = 0;
+        } else {
+            // 非年初但月初且非年末非月末 前取月中间去年后取月天, 如2018-02-01&&2020-11-3
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear;
+            wholeMonth = (12 - startMonth) + endMonth;
+            WholeDay = endDay;
+        }
+    } else {
+        if (endMonth == 11 && endDay == 31) {
+            // 非年初非月初且年末: 前取月天,后取年 ;如2018-02-08&&2020-12-31
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear
+            wholeMonth = 12 - startMonth - 1;
+            WholeDay = moment(startDate).endOf('month').format("DD") - startDay;
+        } else if(endDay == moment(endDate).endOf('month').format("DD")){
+            // 非年初非月初且非年末但月末 前取月天后取月中间取年, 如果当年0,跨1年1,跨2年为2;如2018-02-08&&2020-11-30
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear;
+            wholeMonth = (12 - 12 - startMonth - 1) + (endMonth + 1);
+            WholeDay = moment(startDate).endOf('month').format("DD") - startDay;
+        } else {
+            // 非年初非月初且非年末非月末 前后都取月天中间取年, 如2018-02-08&&2020-11-3
+            wholeYear = endYear > startYear ? endYear - startYear - 1 : endYear - startYear
+            wholeMonth = endYear > startYear ? (12 - startMonth) + endMonth : 12 - startMonth;
+            WholeDay = moment(startDate).endOf('month').format("DD") - endDay;
+        }
+    }
+    return {wholeYear, wholeMonth, WholeDay}
+
+    // let str = 'wholeYear: ' + wholeYear + '  wholeMonth:' + wholeMonth + '  WholeDay: ' + WholeDay
+    // $('#result').text(str);
+}
